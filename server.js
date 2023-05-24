848e2c67000e1c4e8d8d3d3db7688cbfc6ae7b0e
Bro74 <bmwhite1014@gmail.com>
2023-05-24 17:59:45 -0400

    titled workspace

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const sequelize = require('./config/connection')// import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 app.use(express.json());
 
 app.listen(PORT, ()=>{
