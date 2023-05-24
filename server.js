0d72cb719e0b783b89315f091c2eefdbfc311d89
Bro74 <bmwhite1014@gmail.com>
2023-05-24 17:24:43 -0400

    server.js basic logic

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection')// import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.listen(PORT, ()=>{
+    console.log('App Listening on port ${PORT}!')
+});
